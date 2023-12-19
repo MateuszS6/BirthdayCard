ca5f23de7cce75e93694c02f2ce2a7799f49f0a4
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2023-12-19 18:48:09 +0000

    Improved look
    Added alignment and padding using modifier

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 
@@ -25,6 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(to = "Bob", from = "Mateusz")
                 }
             }
         }
@@ -33,15 +39,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(to: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp) // Good practice: padding in increments of 4
+    ) {
+        Text( // Message
             text = "Happy Birthday $to!",
             fontSize = 100.sp,
-            lineHeight = 116.sp
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
-        Text(
+        Text( // Signature
             text = "From $from",
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
